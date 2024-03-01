1f09d8029380a021f19b9561f8eb1d25c408db17
mwarith <35202785+mwarith@users.noreply.github.com>
2024-03-01 04:30:17 +0200

    2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,21 +14,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   File? _image;
+  Image? qrImage;
   String? url;
 
   getImage() async {
-    final picker = ImagePicker();
-    final image = await picker.pickImage(source: ImageSource.camera);
+    var picker = ImagePicker();
+    var image = await picker.pickImage(source: ImageSource.camera);
 
     setState(() {
-      _image = File(image!.path);
+      if (image != null) _image = File(image.path);
     });
     var imageName = basename(image!.path);
-
     var ref = FirebaseStorage.instance.ref(imageName);
     await ref.putFile(_image!);
-
     url = await ref.getDownloadURL();
+    setState(() {
+      qrImage = Image.network(url!);
+    });
   }
 
   @override
@@ -87,13 +90,14 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(
                 height: 10,
               ),
-              SizedBox(
-                height: 550,
-                width: 350,
-                child: _image != null
-                    ? Image.file(_image!)
-                    : const Center(child: Text("Please take a photo!")),
-              ),
+              if (url != null)
+                QrImageView(
+                  version: QrVersions.auto,
+                  size: 200.0,
+                  data: url!,
+                )
+              else
+                (const Text("Please take a photo!")),
             ],
           ),
         ),
